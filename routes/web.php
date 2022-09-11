05204ceb119598f4ede5e72acff5a7787c56ce3f
imai0708 <asano120pnpn@gmail.com>
2022-09-11 11:25:14 +0900

    resourceの作成
    git cm -m  resourceの作成

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('articlec', 
+App\Http\Controllers\ArticController::
+class);
